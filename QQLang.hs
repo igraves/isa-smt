8d0b8d672915704ad4e3dd5bb24eeff880e711a7
Ian Graves <thegravian@gmail.com>
2013-01-18 16:08:55 -0600

    Initial haskell stuff

---
+++ QQLang.hs
@@ -0,0 +1,71 @@
+module QQLang where
+import Text.Parsec
+import qualified Text.Parsec.Token as P
+import Text.Parsec.Language (emptyDef)
+import qualified X86 as X86
+{-
+ - The purpose of this is to fashion a DSL for describing X86 instructions in a way
+ - that is more conducive to literacy.  In other words, we want to be able to pattern
+ - match on *actual written x86 assembly* in addition to assembly descriptors from the
+ - opcode documentation.  Here are some examples:
+ -
+ - and eax, eax
+ - and r/m8, imm8
+ - add r/m16/32, r16/32
+ -
+ - eval [ins|add eax, eax|] = ...
+ -}
+
+lexer = P.makeTokenParser emptyDef 
+identifier = P.identifier lexer
+symbol = P.symbol lexer
+
+popcode = do
+            ident <- identifier
+            case (X86.opMap ident) of
+                    (Just x) -> return x
+                    Nothing  -> fail "Invalid opcode"
+                
+
+register = do            
+                    try(do r <- symbol "al" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "ah" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "bl" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "bh" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "cl" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "ch" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "dl" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "dh" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "ax" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "bx" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "cx" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "dx" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "bp" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "si" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "di" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "sp" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "eax" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "ebx" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "ecx" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "edx" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "ebp" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "esi" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "edi" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "esp" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "rax" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "rbx" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "rcx" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "rdx" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "rbp" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "rsi" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "rdi" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "rsp" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "r8"  ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "r9"  ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "r10" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "r11" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "r12" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "r13" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "r14" ;return $ X86.regMap r) <|>
+                      try(do r <- symbol "r15" ;return $ X86.regMap r)
+
---
+++ X86.hs
@@ -0,0 +1,193 @@
+module X86 where
+import Data.SBV
+import Data.Char
+
+data System = System {
+                          srax :: SInt64
+                        , srbx :: SInt64
+                        , srcx :: SInt64
+                        , srdx :: SInt64
+                        , srbp :: SInt64
+                        , srsi :: SInt64
+                        , srdi :: SInt64
+                        , srsp :: SInt64
+                        , sr8  :: SInt64
+                        , sr9  :: SInt64
+                        , sr10 :: SInt64
+                        , sr11 :: SInt64
+                        , sr12 :: SInt64
+                        , sr13 :: SInt64
+                        , sr14 :: SInt64
+                        , sr15 :: SInt64
+                    }
+
+data Size = B8L | B8H | B16 | B32 | B64 deriving (Show, Eq)
+
+data Register = RAX
+                | RBX
+                | RCX
+                | RDX
+                | RBP
+                | RSI
+                | RDI
+                | RSP
+                | R8
+                | R9
+                | R10
+                | R11
+                | R12
+                | R13
+                | R14
+                | R15 deriving (Show, Eq)
+
+data OpCode =   ADD 
+              | XOR 
+              | MOV 
+
+opMap x = case (map toLower x) of
+                "add" -> Just ADD
+                "xor" -> Just XOR
+                "mov" -> Just MOV
+                _     -> Nothing
+
+data OpSpec =   RegLit Register Size 
+               | Reg Size
+               | RM Size 
+               | Imm Size deriving (Show, Eq)
+
+
+mapRegLit :: Register -> (System -> SInt64)
+mapRegLit RAX = srax
+mapRegLit RBX = srbx
+mapRegLit RCX = srcx
+mapRegLit RDX = srdx
+mapRegLit RBP = srbp
+mapRegLit RSI = srsi
+mapRegLit RDI = srdi
+mapRegLit RSP = srsp
+mapRegLit R8  = sr8
+mapRegLit R9  = sr9
+mapRegLit R10 = sr10
+mapRegLit R11 = sr11
+mapRegLit R12 = sr12
+mapRegLit R13 = sr13
+mapRegLit R14 = sr14
+mapRegLit R15 = sr15
+
+{-
+regWrite :: Register -> System -> (SInt64 -> System)
+regWrite RAX sys = \i -> sys {srax=i}
+regWrite RBX sys = \i -> sys {srbx=i}
+regWrite RCX sys = \i -> sys {srcx=i}
+regWrite RDX sys = \i -> sys {srdx=i}
+regWrite RBP sys = \i -> sys {srbp=i}
+regWrite RSI sys = \i -> sys {srsi=i}
+regWrite RDI sys = \i -> sys {srdi=i}
+regWrite RSP sys = \i -> sys {srsp=i}
+regWrite R8  sys = \i -> sys {sr8=i}
+regWrite R9  sys = \i -> sys {sr9=i}
+regWrite R10 sys = \i -> sys {sr10=i}
+regWrite R11 sys = \i -> sys {sr11=i}
+regWrite R12 sys = \i -> sys {sr12=i}
+regWrite R13 sys = \i -> sys {sr13=i}
+regWrite R14 sys = \i -> sys {sr14=i}
+regWrite R15 sys = \i -> sys {sr15=i}
+
+maskOp (RegLit r B8L) swrd  = swrd .&. 0xFF
+maskOp (RegLit r B8H) swrd  = swrd .&. 0x00FF
+maskOp (RegLit r B16) swrd  = swrd .&. 0xFFFF
+maskOp (RegLit r B32) swrd  = swrd .&. 0xFFFFFFFF
+maskOp (RegLit r B64) swrd  = swrd 
+
+writeRegLit reg@(RegLit r s) swrd sys = let swrd' = maskOp reg swrd
+                                            update = regWrite r sys
+                                         in update swrd'
+
+-}
+regMap x = case (map toLower x) of
+                    "al" ->  al
+                    "ah" ->  ah
+                    "bl" ->  bl
+                    "bh" ->  bh
+                    "cl" ->  cl
+                    "ch" ->  ch
+                    "dl" ->  dl
+                    "dh" ->  dh
+                    "ax" ->  ax
+                    "bx" ->  bx
+                    "cx" ->  cx
+                    "dx" ->  dx
+                    "bp" ->  bp
+                    "si" ->  si
+                    "di" ->  di
+                    "sp" ->  sp
+                    "eax" ->  eax
+                    "ebx" ->  ebx
+                    "ecx" ->  ecx
+                    "edx" ->  edx
+                    "ebp" ->  ebp
+                    "esi" ->  esi
+                    "edi" ->  edi
+                    "esp" ->  esp
+                    "rax" ->  rax
+                    "rbx" ->  rbx
+                    "rcx" ->  rcx
+                    "rdx" ->  rdx
+                    "rbp" ->  rbp
+                    "rsi" ->  rsi
+                    "rdi" ->  rdi
+                    "rsp" ->  rsp
+                    "r8"  ->  r8
+                    "r9"  ->  r9
+                    "r10" ->  r10
+                    "r11" ->  r11
+                    "r12" ->  r12
+                    "r13" ->  r13
+                    "r14" ->  r14
+                    "r15" ->  r15
+
+
+
+al = (RegLit RAX B8L)
+ah = (RegLit RAX B8H)
+bl = (RegLit RBX B8L)
+bh = (RegLit RBX B8H)
+cl = (RegLit RCX B8L)
+ch = (RegLit RCX B8H)
+dl = (RegLit RDX B8L)
+dh = (RegLit RDX B8H)
+
+ax = (RegLit RAX B16)
+bx = (RegLit RBX B16)
+cx = (RegLit RCX B16)
+dx = (RegLit RDX B16)
+bp = (RegLit RBP B16)
+si = (RegLit RSI B16)
+di = (RegLit RDI B16)
+sp = (RegLit RSP B16)
+
+eax = (RegLit RAX B32)
+ebx = (RegLit RBX B32)
+ecx = (RegLit RCX B32)
+edx = (RegLit RDX B32)
+ebp = (RegLit RBP B32)
+esi = (RegLit RSI B32)
+edi = (RegLit RDI B32)
+esp = (RegLit RSP B32)
+
+rax = (RegLit RAX B64)
+rbx = (RegLit RBX B64)
+rcx = (RegLit RCX B64)
+rdx = (RegLit RDX B64)
+rbp = (RegLit RBP B64)
+rsi = (RegLit RSI B64)
+rdi = (RegLit RDI B64)
+rsp = (RegLit RSP B64)
+r8  = (RegLit R8 B64)
+r9  = (RegLit R9 B64)
+r10 = (RegLit R10 B64)
+r11 = (RegLit R11 B64)
+r12 = (RegLit R12 B64)
+r13 = (RegLit R13 B64)
+r14 = (RegLit R14 B64)
+r15 = (RegLit R15 B64)
